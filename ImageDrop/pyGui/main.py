d6c0634f752c701a19ef30ebffc265ffb0750ab3
Jrampoldi <rampoldijourdan@gmail.com>
2024-01-25 20:27:38 -0700

    Added structure for drag and drop application

---
+++ ImageDrop/pyGui/main.py
@@ -0,0 +1,18 @@
+from tkinter import * 
+import ctypes as c
+import os
+from userInterface import *
+
+def main():
+    width = 1920
+    height = 1080
+
+
+#    os.chdir('../')
+
+#    test_lib = c.CDLL("./ImageDrop.so")
+#    test_lib.main()
+    run_gui()
+
+if __name__ == "__main__":
+    main()
